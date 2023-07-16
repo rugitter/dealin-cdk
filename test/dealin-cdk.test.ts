309df61fb4bfaf45344373e66c1e5d3ddb44fc0a
Charlie <csuchq@hotmail.com>
2023-07-16 11:03:55 +1000

    Initial commit

---
+++ test/dealin-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DealinCdk from '../lib/dealin-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dealin-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DealinCdk.DealinCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
